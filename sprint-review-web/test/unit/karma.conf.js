1d7513395ebcfc8814adb75ab42ad3085313f160
Vladimir Pasquier <vpasquier@nuxeo.com>
2018-03-26 23:39:12 -0400

    #1: add war plugin - use yarn - build all packages with stage

--- sprint-review-web/test/unit/karma.conf.js
+++ sprint-review-web/test/unit/karma.conf.js
@@ -3,9 +3,9 @@
 // we are also using it with karma-webpack
 //   https://github.com/webpack/karma-webpack
 
-const webpackConfig = require('../../build/webpack.test.conf');
+var webpackConfig = require('../../build/webpack.test.conf')
 
-module.exports = function karmaConfig(config) {
+module.exports = function karmaConfig (config) {
   config.set({
     // to run in additional browsers:
     // 1. install corresponding karma launcher
@@ -16,18 +16,18 @@ module.exports = function karmaConfig(config) {
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
     preprocessors: {
-      './index.js': ['webpack', 'sourcemap'],
+      './index.js': ['webpack', 'sourcemap']
     },
     webpack: webpackConfig,
     webpackMiddleware: {
-      noInfo: true,
+      noInfo: true
     },
     coverageReporter: {
       dir: './coverage',
       reporters: [
         { type: 'lcov', subdir: '.' },
-        { type: 'text-summary' },
-      ],
-    },
-  });
-};
+        { type: 'text-summary' }
+      ]
+    }
+  })
+}
