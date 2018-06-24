569a55141c9cf3a8fa8b2d35fa5554f5f3218138
Vladimir Pasquier <vpasquier@nuxeo.com>
2018-06-24 16:16:28 -0400

    cosmit

--- sprint-review-server/src/main/kotlin/com/sprintreview/Server.kt
+++ sprint-review-server/src/main/kotlin/com/sprintreview/Server.kt
@@ -1,14 +1,33 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     vpasquier
+ */
 package com.sprintreview
 
+import com.sprintreview.constants.Constants.Companion.INDEX
 import com.sprintreview.constants.Constants.Companion.MODULE_SERVER
 import com.sprintreview.constants.Constants.Companion.PORT
 import com.sprintreview.constants.Constants.Companion.PORT_VALUE
+import com.sprintreview.constants.Constants.Companion.REMOTE_STATIC
+import com.sprintreview.constants.Constants.Companion.RESOURCE_STATIC
 import com.sprintreview.constants.Constants.Companion.SMOKE_TEST
+import com.sprintreview.constants.Endpoints.Companion.ROOT
 import com.sprintreview.constants.Endpoints.Companion.SMOKE
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.content.files
 import io.ktor.content.resource
 import io.ktor.content.resources
 import io.ktor.content.static
@@ -47,10 +66,16 @@ fun Route.endpoints() {
 
 fun Route.staticContent() {
   static {
-    resource("/", "static/index.html")
-    static("/static") {
-      resources("static/static")
+    resource(ROOT, INDEX)
+    static(REMOTE_STATIC) {
+      resources(RESOURCE_STATIC)
     }
+    // TODO externalize those resources in static/static
+//    static(ROOT){
+//      resource("/service-worker.js", "static/service-worker.js")
+//      resource("/favicon.ico", "static/favicon.ico")
+//      resource("/manifest.json", "static/manifest.json")
+//      resources("static")
+//    }
   }
-
 }
--- sprint-review-server/src/main/kotlin/com/sprintreview/constants/Constants.kt
+++ sprint-review-server/src/main/kotlin/com/sprintreview/constants/Constants.kt
@@ -1,10 +1,27 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     vpasquier
+ */
 package com.sprintreview.constants
 
 class Constants {
   companion object {
     const val SMOKE_TEST = "Hello Smoke!"
-    const val RESOURCE_PATH = "static"
-    const val INDEX = "index.html"
+    const val REMOTE_STATIC = "/static"
+    const val RESOURCE_STATIC = "static/static"
+    const val INDEX = "static/index.html"
     const val PORT = "port"
     const val PORT_VALUE = "8080"
     const val MODULE_SERVER = "sprint-review-server"
