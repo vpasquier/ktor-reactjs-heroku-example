0497e06918b31b261bb9f93acb6719ebeb0c91ca
Vladimir Pasquier <vpasquier@nuxeo.com>
2018-06-24 15:57:43 -0400

    #34: upgrade ktor - fix resources serving issue

--- sprint-review-server/src/main/kotlin/com/sprintreview/Server.kt
+++ sprint-review-server/src/main/kotlin/com/sprintreview/Server.kt
@@ -1,44 +1,56 @@
 package com.sprintreview
 
-import com.sprintreview.constants.Constants
-import com.sprintreview.constants.Constants.Companion.RESOURCE_PATH
+import com.sprintreview.constants.Constants.Companion.MODULE_SERVER
+import com.sprintreview.constants.Constants.Companion.PORT
+import com.sprintreview.constants.Constants.Companion.PORT_VALUE
+import com.sprintreview.constants.Constants.Companion.SMOKE_TEST
 import com.sprintreview.constants.Endpoints.Companion.SMOKE
 import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.content.defaultResource
+import io.ktor.application.install
 import io.ktor.content.files
+import io.ktor.content.resource
+import io.ktor.content.resources
 import io.ktor.content.static
-import io.ktor.content.staticBasePackage
+import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
+import io.ktor.routing.Route
 import io.ktor.routing.get
-import io.ktor.routing.route
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import java.io.File
 
 fun main(args: Array<String>) {
-  val portVar: String = System.getenv("PORT") ?: "8080"
-  embeddedServer(Netty, watchPaths = listOf("sprint-review-server"),
+  val portVar: String = System.getenv(PORT) ?: PORT_VALUE
+  embeddedServer(Netty, watchPaths = listOf(MODULE_SERVER),
           port = portVar.toInt(), module = Application::server).apply {
     start(wait = true)
   }
 }
 
 fun Application.server() {
-  val staticFilesDir = File(RESOURCE_PATH)
+  install(DefaultHeaders)
+//  install(CallLogging)
   routing {
-    get(SMOKE) {
-      call.respondText(Constants.SMOKE_TEST, ContentType.Text.Plain)
-    }
-    static {
-      staticBasePackage = "static"
-      defaultResource("index.html")
-      route("static") {
-        files(staticFilesDir)
-      }
-    }
+    //    trace { application.log.trace(it.buildText()) }
+    endpoints()
+    staticContent()
   }
 }
 
+fun Route.endpoints() {
+  get(SMOKE) {
+    call.respondText(SMOKE_TEST, ContentType.Text.Plain)
+  }
+}
+
+fun Route.staticContent() {
+  static {
+    resource("/", "static/index.html")
+    static("/static") {
+      resources("static/static")
+    }
+  }
+
+}
