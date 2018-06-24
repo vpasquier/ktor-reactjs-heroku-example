59feaa919719ad208d03e0802809231aaf765b16
Vladimir Pasquier <vpasquier@nuxeo.com>
2018-06-24 15:57:43 -0400

    #34: upgrade ktor - fix resources serving issue

--- sprint-review-server/src/main/kotlin/com/sprintreview/constants/Constants.kt
+++ sprint-review-server/src/main/kotlin/com/sprintreview/constants/Constants.kt
@@ -3,6 +3,10 @@ package com.sprintreview.constants
 class Constants {
   companion object {
     const val SMOKE_TEST = "Hello Smoke!"
-    const val RESOURCE_PATH = "src/main/resources/static"
+    const val RESOURCE_PATH = "static"
+    const val INDEX = "index.html"
+    const val PORT = "port"
+    const val PORT_VALUE = "8080"
+    const val MODULE_SERVER = "sprint-review-server"
   }
 }
